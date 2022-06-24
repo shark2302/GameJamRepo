d56ace52b0c0c6a9807e95346347804f1ac03d38
e.kravchenko <e.kravchenko@corp.mail.ru>
2022-06-25 00:55:06 +0300

    преследование камеры

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+	public class FollowCamera : MonoBehaviour
+	{
+		[SerializeField] 
+		private GameObject FollowedObject;
+
+		private void Update()
+		{
+			var position = FollowedObject.transform.position;
+			transform.position = new Vector3(position.x, position.y, transform.position.z);
+		}
+	}
+}
